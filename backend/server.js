b6d646a53d28994a197f42689a47263e8c7a900b
Alan <thomasfraiz@gmail.com>
2026-05-22 11:22:31 -0300

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,101 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { Pool, Connection } = require("pg");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new pool({
+    ConnectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL ? {rejectUnauthorized: false} : false,
+});
+
+async function initDB() {
+    try{
+        await pool.query(`        
+            CREATE TABLE ID NOT EXISTS produtos(
+                id SERIAL PRIMARY KEY,
+                nome VARCHAR(255), NOT NULL,
+                quantidade INTEGER NOT NULL,
+                preco NUMERIC (10,2) NOT NULL
+            )
+        `);
+            console.log("Tabela verificada/criada com sucesso");
+        } catch(err){
+            console.error("Error ao criar a tabela:", err);
+    }
+}
+
+initDB(); //chama a função para ela rodar e funcionar quando rodar o servidor local
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h2> API de Controle de Estoque <h2>
+        <p>API functionando corretamente!<p>
+        <a href="/api/produtos">Ver Produtos<a>`)
+})//teste do rota no frontend
+
+const BASE_URL = "/api/produtos";
+    app.get(BASE_URL, async(req,res) => {
+        try{
+            const result = await pool.query(
+                "SELECT * FROM produtos ORDER BY id DESC"
+            );
+            res.json(result,rows);
+        }catch(err) {
+            res.status(500).json({error: "Erro ao buscar produtos"});
+        }
+    });
+    
+app.post(BASE_URL, async(req, res) =>{
+    try{
+        const { nome, quantidade, preco } = req.body;
+
+        const result = await pool.query(
+            "INSERT INTO produtos (nome, quantidade, preco) VALUES($1, $2, $3) RETURNING *",
+            [nome, quantidade, preco]
+        )
+        res.status(201).json(result.rows[0]);
+    }catch(err){
+        res.status(500).json({error: "Erro ao inserir produto"});
+    }
+})
+
+app.put{`${BASE_URL}/id`, async(req,res) => {
+try{
+    const { id } = req.params; 
+    const { nome, produto, preco} = req.body;
+
+    const result = await pool.query(
+        "UPDATE produtos SET nome=$1, quantidade=$2, preco=$3, WHERE id=$4 RETURNING *",
+        [nome, quantidade, preco, id]
+
+    );
+    res.json(result.rows[0]);
+}catch(err){
+    res.status(500).json({
+        error: "Erro ao atualizar o produto"
+app.put(`${BASE_URL}/:id`, async(req, res) =>{
+    const result = await pool.query(
+    ),
+})
+        
+    });
+}
+
+app.delete(`${BASE_URL}/:id`, async(req, res) => {
+    try{
+        const {id} = req.params;
+
+        await pool.query("DELETE FROM produto WHERE id=$1", [id]);
+        res.json({ message: "Produto removido com sucesso!"});
+    }catch(err){
+        res.status(500).json({ error: "Erro ao remover produto"})
+    }
+});
+
+app.listen(PORT, () => console.log(`Servidor rodando em https://localhost:${PORT}`));
